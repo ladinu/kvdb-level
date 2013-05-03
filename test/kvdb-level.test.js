933a240a49d3761ba0f08a5b62438d48bf6d4fec
Ladinu Chandrasinghe <ladinu@gmail.com>
2013-05-02 23:39:54 -0700

    decided to use tap instead of mocha

--- test/kvdb-level.test.js
+++ test/kvdb-level.test.js
@@ -3,57 +3,75 @@ var os        = require('os');
 var path      = require('path');
 var uuid      = require('uuid');
 var levelup   = require('levelup');
-var store     = require('../');
+var test      = require('tap').test;
+var store;
 
-var testdir = uuid.unparse(uuid.v4(null, new Buffer(16), 0)) + '/';
-var tmpdir  = path.join(os.tmpdir(), testdir);
 
-var dbpath = function(name) {
-  return path.join(tmpdir, name);
-}
-
-describe("ctor", function() {
-
-  it("should create a db", function(done) {
-    var db = store( {'dbname': '../db/testdb1'} );
-    
-    db.on('ready', function() {
-      done(assert.ok(db.db.isOpen));
-    })
-
-    db.on('error', function(err) {
-      done(err);
-    });
+test("load module kvdb-level", function(t) {
+  t.doesNotThrow(function() {
+    store = require('../');
   });
-
-  it("should create db with specified name", function() {
-    throw new Error("IMPLEMENT");
-  });
-
-  it("should create a db when no name is given", function() {
-    throw new Error("IMPLEMENT");
-  });
-
-  it("should create a db in specefied path", function() {
-    throw new Error("IMPLEMENT");
-  });
-
-  it("should create a db in cwd when path not given", function() {
-    throw new Error("IMPLEMENT");
-  });
-
+  t.end();
 });
 
-describe("#put", function() {
-  it("should store data", function() {
-    throw new Error("IMPLEMENT");
+
+test("database creation test", function(t) {
+  
+  t.test("should create a db", function(t) {
+
   });
 
-  it("should store piped data", function() {
-    throw new Error("IMPLEMENT");
+  t.test("db name should be setable", function(t) {
+
   });
 
-  it("should throw errors by callback or streams", function() {
-    throw new Error("IMPLEMENT");
+  t.test("when name not given, db must be created with random name", function(t) {
+
   });
 });
+
+// describe("ctor", function() {
+// 
+//   it("should create a db", function(done) {
+//     var db = store( {'dbname': './db/testdb1.db'} );
+//     
+//     db.on('ready', function() {
+//       done(assert.ok(db.db.isOpen));
+//     })
+// 
+//     db.on('error', function(err) {
+//       done(err);
+//     });
+//   });
+// 
+//   it("should create db with specified name", function() {
+//     throw new Error("IMPLEMENT");
+//   });
+// 
+//   it("should create a db when no name is given", function() {
+//     throw new Error("IMPLEMENT");
+//   });
+// 
+//   it("should create a db in specefied path", function() {
+//     throw new Error("IMPLEMENT");
+//   });
+// 
+//   it("should create a db in cwd when path not given", function() {
+//     throw new Error("IMPLEMENT");
+//   });
+// 
+// });
+// 
+// describe("#put", function() {
+//   it("should store data", function() {
+//     throw new Error("IMPLEMENT");
+//   });
+// 
+//   it("should store piped data", function() {
+//     throw new Error("IMPLEMENT");
+//   });
+// 
+//   it("should throw errors by callback or streams", function() {
+//     throw new Error("IMPLEMENT");
+//   });
+// });
